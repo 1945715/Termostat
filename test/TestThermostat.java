f268fa74113555abd83f010e3343cd97619bd2d8
Hakim-Anis Hamani <hamanianis1999@gmail.com>
2021-12-08 17:22:58 -0500

    Test en une classe

--- test/TestThermostat.java
+++ test/TestThermostat.java
@@ -11,109 +11,31 @@ public class TestThermostat{
         ServiceTemperature mockServiceTemperature = mock(ServiceTemperature.class);
         ControleThermostat controleur =  new ControleThermostat();
 
-        when(mockServiceTemperature.getTemperature()).thenReturn(20.0);
-
-
         controleur.setServiceThermostat(mockServiceThermostat);
         controleur.setserviceTemperature(mockServiceTemperature);
 
+        when(mockServiceTemperature.getTemperature()).thenReturn(20.0);
         assertEquals("20.0 OK - Démarrer chauffage",controleur.controlageThermostat());
 
-
-        verify(mockServiceThermostat, times(1)).dermarrerChauffage();
-        verify(mockServiceTemperature, times(1)).getTemperature();
-    }
-
-    @Test
-    public void testExemple2() {
-
-        ServiceThermostat mockServiceThermostat = mock(ServiceThermostat.class);
-        ServiceTemperature mockServiceTemperature = mock(ServiceTemperature.class);
-        ControleThermostat controleur =  new ControleThermostat();
-
         when(mockServiceTemperature.getTemperature()).thenReturn(22.0);
-        controleur.setTemperature(20.0);
-
-        controleur.setServiceThermostat(mockServiceThermostat);
-        controleur.setserviceTemperature(mockServiceTemperature);
-
         assertEquals("Refusé", controleur.controlageThermostat());
-
-    }
-    @Test
-    public void testExemple3() {
-
-        ServiceThermostat mockServiceThermostat = mock(ServiceThermostat.class);
-        ServiceTemperature mockServiceTemperature = mock(ServiceTemperature.class);
-        ControleThermostat controleur =  new ControleThermostat();
 
         when(mockServiceTemperature.getTemperature()).thenReturn(20.5);
-        controleur.setTemperature(20.0);
-
-        controleur.setServiceThermostat(mockServiceThermostat);
-        controleur.setserviceTemperature(mockServiceTemperature);
-
         assertEquals("20.5 OK - Démarrer chauffage", controleur.controlageThermostat());
 
-        verify(mockServiceThermostat, times(1)).dermarrerChauffage();
-        verify(mockServiceTemperature, times(3)).getTemperature();
-    }
-    @Test
-    public void testExemple4() {
-
-        ServiceThermostat mockServiceThermostat = mock(ServiceThermostat.class);
-        ServiceTemperature mockServiceTemperature = mock(ServiceTemperature.class);
-        ControleThermostat controleur =  new ControleThermostat();
-
         when(mockServiceTemperature.getTemperature()).thenReturn(21.0);
-        controleur.setTemperature(20.5);
-
-        controleur.setServiceThermostat(mockServiceThermostat);
-        controleur.setserviceTemperature(mockServiceTemperature);
-
         assertEquals("21.0 OK - Arrêter chauffage", controleur.controlageThermostat());
 
-        verify(mockServiceThermostat, times(1)).arreterChauffage();
-        verify(mockServiceTemperature, times(3)).getTemperature();
-
-
-    }
-    @Test
-    public void testExemple5() {
-
-        ServiceThermostat mockServiceThermostat = mock(ServiceThermostat.class);
-        ServiceTemperature mockServiceTemperature = mock(ServiceTemperature.class);
-        ControleThermostat controleur =  new ControleThermostat();
-
         when(mockServiceTemperature.getTemperature()).thenReturn(19.0);
-        controleur.setTemperature(21.0);
-
-        controleur.setServiceThermostat(mockServiceThermostat);
-        controleur.setserviceTemperature(mockServiceTemperature);
-
         assertEquals("Refusé", controleur.controlageThermostat());
 
-
-
-    }
-    @Test
-    public void testExemple6() {
-
-        ServiceThermostat mockServiceThermostat = mock(ServiceThermostat.class);
-        ServiceTemperature mockServiceTemperature = mock(ServiceTemperature.class);
-        ControleThermostat controleur =  new ControleThermostat();
-
         when(mockServiceTemperature.getTemperature()).thenReturn(21.5);
-        controleur.setTemperature(21.0);
-
-        controleur.setServiceThermostat(mockServiceThermostat);
-        controleur.setserviceTemperature(mockServiceTemperature);
-
         assertEquals("21.5 OK - Arrêter chauffage", controleur.controlageThermostat());
 
-        verify(mockServiceThermostat, times(1)).arreterChauffage();
-        verify(mockServiceTemperature, times(3)).getTemperature();
-
-
+        verify(mockServiceThermostat, times(2)).arreterChauffage();
+        verify(mockServiceThermostat, times(2)).dermarrerChauffage();
+        verify(mockServiceTemperature, times(13)).getTemperature();
     }
+
+
 }
